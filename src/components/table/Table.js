31e504f05c802da2edb064fe201ee3102f48c8e6
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2022-03-12 12:57:40 +0300

    finished resizing

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
-import {$} from '@core/dom'
+import {shouldResize} from '@/components/table/table.functions'
+import {resizeHandler} from '@/components/table/table.resize'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -16,49 +17,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target)
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-      const type = $resizer.data.resize
-      const sideProp = type === 'col' ? 'bottom' : 'right'
-      let value = null
-
-      $resizer.css({
-        opacity: 1,
-        [sideProp]: '-2000px'
-      })
-
-      document.onmousemove = e => {
-        if (type === 'col') {
-          const delta = e.pageX - coords.right
-          value = Math.floor(coords.width + delta)
-          $resizer.css({right: -delta + 'px'})
-        } else {
-          // coords.bottom - нижняя граница строчки а не отсчитывание координат от нижней границы экрана (указываемое число в sideProp это количество пикселей которое нужно отступить от границы родителя(нулевых значений), поэтому чтобы выйти за его пределы нужно назначить отрицательное значение)
-          const delta = Math.floor(e.pageY - coords.bottom)
-          value = Math.floor(coords.height + delta)
-          $resizer.css({bottom: -delta + 'px'})
-        }
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null
-        document.onmouseup = null
-
-        if (type === 'col') {
-          this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-            .forEach(element => element.style.width = value + 'px')
-        } else {
-          $parent.css({height: value + 'px'})
-        }
-
-        $resizer.css({
-          opacity: 0,
-          bottom: 0,
-          right: 0,
-        })
-      }
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -41,18 +41,18 @@ export function createTable(rowsCount = 15) {
   const rows = []
 
   const cols = new Array(colsCount)
-    .fill('')
-    .map(toChar)
-    .map(toColumn)
-    .join('')
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('')
 
   rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
-      .fill('')
-      .map(toCell)
-      .join('')
+        .fill('')
+        .map(toCell)
+        .join('')
 
     rows.push(createRow(i + 1, cells))
   }
